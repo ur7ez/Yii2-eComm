4659b0ba90d536d7bccae2025d898c092c21bedc
ur7ez <ur7ez@ukr.net>
2023-06-21 14:27:36 +0300

    start implementing cart item checkout page

--- frontend/views/cart/checkout.php
+++ frontend/views/cart/checkout.php
@@ -7,6 +7,7 @@
 
 use yii\bootstrap5\ActiveForm;
 ?>
+<script src="https://www.paypal.com/sdk/js?client-id=AU0al8Aj960qjY5gpCCnoof3Jf7eLAM5CIoirkGrMxeYiuv5BQl_tvf93MUTN76Vxsxx1JD01dwvBuac&currency=USD"></script>
 <?php $form = ActiveForm::begin([
     'action' => [''],
 ]); ?>
@@ -57,12 +58,65 @@ use yii\bootstrap5\ActiveForm;
                         <td class="text-end"><?= Yii::$app->formatter->asCurrency($totalPrice) ?></td>
                     </tr>
                 </table>
-                <p class="mt-3 text-end">
-                    <button class="btn btn-secondary">Checkout</button>
-                </p>
+
+                <div id="paypal-button-container"></div>
+<!--                <p class="mt-3 text-end">-->
+<!--                    <button class="btn btn-secondary">Checkout</button>-->
+<!--                </p>-->
             </div>
         </div>
     </div>
 </div>
 
 <?php ActiveForm::end(); ?>
+<script>
+    paypal.Buttons(
+        // {
+        //     // Order is created on the server and the order id is returned
+        //     createOrder() {
+        //         return fetch("/my-server/create-paypal-order", {
+        //             method: "POST",
+        //             headers: {
+        //                 "Content-Type": "application/json",
+        //             },
+        //             // use the "body" param to optionally pass additional order information
+        //             // like product skus and quantities
+        //             body: JSON.stringify({
+        //                 cart: [
+        //                     {
+        //                         sku: "YOUR_PRODUCT_STOCK_KEEPING_UNIT",
+        //                         quantity: "YOUR_PRODUCT_QUANTITY",
+        //                     },
+        //                 ],
+        //             }),
+        //         })
+        //             .then((response) => response.json())
+        //             .then((order) => order.id);
+        //     },
+            // Finalize the transaction on the server after payer approval
+        //     onApprove(data) {
+        //         return fetch("/my-server/capture-paypal-order", {
+        //             method: "POST",
+        //             headers: {
+        //                 "Content-Type": "application/json",
+        //             },
+        //             body: JSON.stringify({
+        //                 orderID: data.orderID
+        //             })
+        //         })
+        //             .then((response) => response.json())
+        //             .then((orderData) => {
+        //                 // Successful capture! For dev/demo purposes:
+        //                 console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+        //                 const transaction = orderData.purchase_units[0].payments.captures[0];
+        //                 alert(`Transaction ${transaction.status}: ${transaction.id}\n
+        //             \nSee console for all available details`);
+        //                 // When ready to go live, remove the alert and show a success message within this page. For example:
+        //                 // const element = document.getElementById('paypal-button-container');
+        //                 // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+        //                 // Or go to another URL:  window.location.href = 'thank_you.html';
+        //             });
+        //     }
+        // }
+    ).render('#paypal-button-container');
+</script>
