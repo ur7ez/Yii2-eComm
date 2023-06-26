00195a27aebb7c728d9b2ad5f8f209bd5accf6e6
ur7ez <ur7ez@ukr.net>
2023-06-26 15:04:04 +0300

    fix page styling

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -8,7 +8,6 @@ use yii\bootstrap5\Html;
 use yii\bootstrap5\ActiveForm;
 
 $this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
     <div class="row justify-content-center">
--- frontend/views/site/resetPassword.php
+++ frontend/views/site/resetPassword.php
@@ -1,5 +1,4 @@
 <?php
-
 /** @var yii\web\View $this */
 /** @var yii\bootstrap5\ActiveForm $form */
 /** @var \frontend\models\ResetPasswordForm $model */
@@ -8,23 +7,17 @@ use yii\bootstrap5\Html;
 use yii\bootstrap5\ActiveForm;
 
 $this->title = 'Reset password';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-reset-password">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please choose your new password:</p>
-
-    <div class="row">
-        <div class="col-lg-5">
+    <div class='row justify-content-center'>
+        <div class='col-lg-5'>
+            <h1><?= Html::encode($this->title) ?></h1>
+            <p>Please choose your new password:</p>
             <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
-
-                <?= $form->field($model, 'password')->passwordInput(['autofocus' => true]) ?>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
-                </div>
-
+            <?= $form->field($model, 'password')->passwordInput(['autofocus' => true]) ?>
+            <div class="form-group">
+                <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
+            </div>
             <?php ActiveForm::end(); ?>
         </div>
     </div>
