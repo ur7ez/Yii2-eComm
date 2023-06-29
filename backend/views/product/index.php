8c45f980d2f7d23f06322e4f32876e24f523a56c
ur7ez <ur7ez@ukr.net>
2023-06-29 22:26:10 +0300

    clean up Navbar on backend

--- backend/views/product/index.php
+++ backend/views/product/index.php
@@ -1,5 +1,4 @@
 <?php
-
 use common\models\Product;
 use yii\helpers\Html;
 use yii\helpers\Url;
@@ -14,15 +13,11 @@ $this->title = 'Products';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="product-index">
-
     <h1><?= Html::encode($this->title) ?></h1>
-
     <p>
         <?= Html::a('Create Product', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
@@ -63,12 +58,11 @@ $this->params['breadcrumbs'][] = $this->title;
             //'updated_by',
             [
                 'class' => ActionColumn::class,
+                'contentOptions' => ['style' => 'width:90px'],
                 'urlCreator' => function ($action, Product $model, $key, $index, $column) {
                     return Url::toRoute([$action, 'id' => $model->id]);
                  }
             ],
         ],
-    ]); ?>
-
-
+    ]) ?>
 </div>
