e9f4fee1379f3d184164d27d8e0e1b115960a71b
ur7ez <ur7ez@ukr.net>
2023-07-02 22:03:03 +0300

    start internalization

--- common/config/main.php
+++ common/config/main.php
@@ -2,22 +2,24 @@
 require_once __DIR__ . '/../../common/helpers.php';
 
 return [
+    'language' => 'uk-UA',
+    'sourceLanguage' => 'en-US',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'vendorPath' => dirname(__DIR__, 2) . '/vendor',
     'components' => [
         'cache' => [
             'class' => \yii\caching\FileCache::class,
         ],
         'formatter' => [
             'class' => \common\i18n\Formatter::class,
-            'dateFormat' => 'dd.MM.yyyy',
             'datetimeFormat' => 'php:d.m.Y H:i',
+            'dateFormat' => 'dd.MM.yyyy',
             'decimalSeparator' => ',',
             'thousandSeparator' => ' ',
-            'currencyCode' => 'USD',
+//            'currencyCode' => 'USD',
         ],
     ],
 ];
